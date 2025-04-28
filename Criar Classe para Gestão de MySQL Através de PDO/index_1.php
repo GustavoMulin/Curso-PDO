2e9e5771774c9fa9bda425c9c9595d2f26ab4116
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-04-28 11:47:50 -0400

    News Files

--- Criar Classe para Gestão de MySQL Através de PDO/index_1.php
+++ Criar Classe para Gestão de MySQL Através de PDO/index_1.php
@@ -10,3 +10,29 @@ $username = 'root';
 
 // ligação
 $ligacao = new PDO("mysql:host=localhost;dbname=$database", $username);
+
+// carregar os dados (em formato de array de objetos)
+$resultados = $ligacao->query("SELECT * FROM clientes LIMIT 20")->fetchAll(PDO::FETCH_OBJ);
+
+// fecha a ligação
+$ligacao = null;
+
+/*
+Imagina que vais ter que conectar várias vezes à base de dados!
+Vamos melhorar o nosso código. Vamos torná-lo modelar.
+Vamos fazer ainda melhor: vamos criar uma classe que pode ser
+usada em qualquer projeto.
+
+Quermos:
+- uma classe que possa ser usada em qualquer projeto
+- que permita efetuar todas as operações CRUD SQL
+- que permita fazer queries básicas ou parametrizadas
+- que permita configurar a ligação, e a forma como os dados são devolvidos
+- que controle erros e nos devolva essa informação
+- que nos possa devolver outras informações relevantes
+
+IMPORTANTE: Este exercício é um bom ponto de partida para criares a tua
+própria classe. Podes usar este código como ponto inicial.
+vai ficar completamente funcional mas NÂO DEVE USAR EM PRODUÇÃO.
+
+*/
---
+++ Criar uma Base de Dados com PHP/index_1.php
@@ -0,0 +1,25 @@
+<?php
+
+// CRIAR UMA BASE DE DADOS COM PHP
+
+/*
+Já te disse que vai, na esmagadora maioria dos casos,
+usar uma framework de PHP para criar as tuas aplicações.
+Essas framework já contém mecanismos internos para permitir
+a criação de base de dados.
+
+Ainda assim, vamos ver como pode criar uma base de dados
+através de PHP e PDO.
+
+Vamos voltar ao modelo inicial do nosso código.
+Na definição de uma conexão PDO e parâmetro que contém p
+nome da base de dados não é obrigatório.
+
+Neste caso, vamos usar uma conexão ligeiramente diferente
+para podermos criar a base de dados.
+
+E para simplificar o processo, vamos usar o utilizadoe root
+do MySQL.
+
+IMPORTANTE: DEVES SEMPRE CRIAR UMA PASSWORD PARA O USER ROOT
+*/
